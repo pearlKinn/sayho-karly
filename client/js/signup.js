b0cf087526529ff70ddbe1aa8b50ff7b25d10d8b
pearlKinn <jinju000202@naver.com>
2023-07-30 18:30:05 +0900

    [Chore] : config 파일 수정 [Modify] : signup html 수정 ( <a> -> <button>), 및 프리텐다드 링크 추가,  tailwind.css font family 추가,

--- client/js/signup.js
+++ client/js/signup.js
@@ -1,44 +1,44 @@
-import {addClass, getNode, removeClass} from '../lib/index.js'
+import { addClass, getNode, removeClass, saveStorage } from '../lib/index.js';
 
-const authBtn = getNode('.authBtn')
-const IdInput = getNode('.userIdInput')
-const nameInput = getNode('.userNameInput')
+const authBtn = getNode('.authBtn');
+const IdInput = getNode('.userIdInput');
+const nameInput = getNode('.userNameInput');
 const emailInput = getNode('.userEmailInput');
 const PhoneInput = getNode('.userPhoneInput');
+const searchAddress = getNode('.searchAddressBtn');
 const passwordInput = getNode('.userPasswordInput');
+const checkDuplicate = getNode('.checkDuplicateBtn');
 const passwordCheckInput = getNode('.userCheckInput');
 
-// const errorMessages = getNodes('.errorMessage');
-
-function validateInput(inputElement, validationFunction , target) {
+function validateInput(inputElement, validationFunction, target) {
   if (validationFunction(inputElement.value) || !inputElement.value) {
-    addClass(target, 'hidden')
+    addClass(target, 'hidden');
   } else {
-    removeClass(target, 'hidden')
+    removeClass(target, 'hidden');
   }
 }
 
-function handleCheckPwd(e){
-  const errorMessage = e.target.nextElementSibling
-  if(passwordInput.value !== passwordCheckInput.value) {
-    removeClass(errorMessage, 'hidden')
-  }else {
-    addClass(errorMessage, 'hidden')
+function handleCheckPwd(e) {
+  const errorMessage = e.target.nextElementSibling;
+  if (passwordInput.value !== passwordCheckInput.value) {
+    removeClass(errorMessage, 'hidden');
+  } else {
+    addClass(errorMessage, 'hidden');
   }
-
 }
 function addValidateForInputEvent(input, inputReg) {
-  return input.addEventListener("input", () => validateInput(input, inputReg, input.nextElementSibling));
+  return input.addEventListener('input', () =>
+    validateInput(input, inputReg, input.nextElementSibling)
+  );
 }
 
 function handleCheckValidation(e) {
-  const errorMessage = e.target.nextElementSibling
-  if(hasNumber(nameInput.value)) {
-    removeClass(errorMessage, 'hidden')
-  }else {
-    addClass(errorMessage, 'hidden')
+  const errorMessage = e.target.nextElementSibling;
+  if (hasNumber(nameInput.value)) {
+    removeClass(errorMessage, 'hidden');
+  } else {
+    addClass(errorMessage, 'hidden');
   }
-
 }
 
 function handleCheckNumeric(e) {
@@ -49,13 +49,30 @@ function handleCheckNumeric(e) {
   // 숫자만 남은 값을 input 요소에 설정합니다.
   e.target.value = numericValue;
 
-  if(e.target.value) {
-    removeClass(authBtn,'disabled:border-gray-300')
-    removeClass(authBtn, 'disabled:font-semibold')
-    removeClass(authBtn,'disabled:text-gray-300')
+  if (e.target.value) {
+    removeClass(authBtn, 'disabled:border-gray-300');
+    removeClass(authBtn, 'disabled:font-semibold');
+    removeClass(authBtn, 'disabled:text-gray-300');
+  } else {
+    addClass(authBtn, 'disabled:border-gray-300');
+    addClass(authBtn, 'disabled:font-semibold');
+    addClass(authBtn, 'disabled:text-gray-300');
   }
 }
 
+
+// function handleCreateNum() {
+//   let key =  emailInput.value
+//   saveStorage(,{generateRandomID(10)})
+  
+// }
+
+
+function openLinkInPopup(e) {
+  e.preventDefault();
+  const url = 'https://www.kurly.com/address/shipping-address';
+  window.open(url, '_blank', 'width=500, height=400');
+}
 
 function hasNumber(text) {
   if (typeof text !== 'string') {
@@ -66,25 +83,27 @@ function hasNumber(text) {
   return regex.test(text);
 }
 
-function emailReg(text){
+function emailReg(text) {
+  const RegExr =
   // eslint-disable-next-line no-useless-escape
-  const RegExr = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  
-  return RegExr.test(String(text).toLowerCase())
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  return RegExr.test(String(text).toLowerCase());
 }
 
-function pwReg(text){
+function pwReg(text) {
   const RegExr = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^*+=-]).{6,16}$/;
   return RegExr.test(String(text).toLowerCase());
-
 }
 
 passwordCheckInput.addEventListener('input', handleCheckPwd);
-nameInput.addEventListener('input', handleCheckValidation)
+nameInput.addEventListener('input', handleCheckValidation);
 PhoneInput.addEventListener('input', handleCheckNumeric);
-addValidateForInputEvent(IdInput, emailReg)
-addValidateForInputEvent(passwordInput, pwReg)
-addValidateForInputEvent(emailInput, emailReg)
+searchAddress.addEventListener('click', openLinkInPopup);
+// checkDuplicate.addEventListener('click', handleCreateNum);
+addValidateForInputEvent(IdInput, emailReg);
+addValidateForInputEvent(passwordInput, pwReg);
+addValidateForInputEvent(emailInput, emailReg);
 
 function generateRandomID(length) {
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
@@ -98,7 +117,6 @@ function generateRandomID(length) {
   return randomID;
 }
 
-
 // const generateRandomID = (length) => {
 //   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
 //   let randomID = '';
@@ -110,4 +128,3 @@ function generateRandomID(length) {
 
 //   return randomID;
 // }
-
